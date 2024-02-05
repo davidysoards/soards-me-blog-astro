7a7acc9c6964b92b8f88370d429f9c8c3fdf847d
David Soards <david@soards.me>
2024-02-05 03:29:17 -0500

    radix colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
@@ -8,7 +9,26 @@ export default {
 				sans: '"HelveticaNeue", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
 				serif: 'Charter, Georgia, serif',
 			},
+			colors: {
+				accent: generateScale('red'),
+				gray: generateScale('mauve'),
+			},
 		},
 	},
 	plugins: [],
 };
+
+function generateScale(name) {
+	let scale = Array.from({ length: 12 }, (_, i) => {
+		let id = i + 1;
+		return [id, `var(--${name}-${id})`];
+	});
+	// to generate alpha and solid values
+	// return [
+	// [id, `var(--${name}-${id})`],
+	// [`a${id}`, `var(--${name}-a${id})`],
+	// ];
+	// }).flat();
+
+	return Object.fromEntries(scale);
+}
