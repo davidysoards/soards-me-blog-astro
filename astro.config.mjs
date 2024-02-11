dfb76268a3fd23d367bd1d4d91400568dbca55ca
David Soards <david@soards.me>
2024-02-11 00:45:26 -0500

    remove alpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,21 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import Icons from 'unplugin-icons/vite';
 
-import alpinejs from "@astrojs/alpinejs";
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: false
-  }), alpinejs()],
-  vite: {
-    plugins: [Icons({
-      compiler: 'astro'
-    })]
-  }
+	site: 'https://example.com',
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	vite: {
+		plugins: [
+			Icons({
+				compiler: 'astro',
+			}),
+		],
+	},
 });
