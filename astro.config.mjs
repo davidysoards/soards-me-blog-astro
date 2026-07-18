9adea58f743964b31591da0f8d1b2251a9bc32fd
David Soards <david@soards.me>
2026-07-17 21:36:12 -0400

    upgrade Astro to v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://blog.soards.me',
-	integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false })],
+	integrations: [mdx(), sitemap()],
 	vite: {
-		plugins: [],
+		plugins: [tailwindcss()],
 	},
 	// markdown: {
 	// 	shikiConfig: {
